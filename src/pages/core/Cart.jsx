24eaeb89ea43596e5d0f54749e40fb024efb2ee1
bildan <bildanjauhary@gmail.com>
2026-06-15 00:41:44 +0700

    feat: add first structure for layouting cart page

---
+++ src/pages/core/Cart.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/layouts/MainLayout";
+
+export default function Cart(){
+  return(
+    <MainLayout>
+      <div className="w-[83%] mt-3">
+
+      </div>
+    </MainLayout>
+  )
+}
